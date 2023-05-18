155ec42ba4efb400a02f4d469459f0526d79f638
Junhee Yoo <ihavnoid@yahoo.com>
2023-05-18 20:15:58 +0900

    Added "AI table" feature
    type !!aitable ## and it will autopoulate AI compilation

--- tmm.js
+++ tmm.js
@@ -1,11 +1,9 @@
 __serverBase__ = "http://192.168.233.234/tmm/";
 __prefix__ = "tmm_edit_";
 __retry_period__ = 500;
-let editor;
+let editor = null;
 let last_timestamp = 0;
 let rwkey = null;
-
-let ai_table = new Map();
 
 let popupWindow = window;
 
@@ -50,9 +48,6 @@ function addVisitedPages() {
     // console.log(setting);
 }
 
-function getNextAINum() {
-    return 0;
-}
 
 function timeFormat () {
     let date = new Date();
@@ -69,47 +64,130 @@ function setSize() {
     } );
 }
 
-function updateChildWindow() {
-    let d = html_to_text.convert(editor.getData(), {wordwrap:9999999});
-    if(getWindow() != window) {
-        getWindow().msgpane.innerHTML = "<pre>" + d + "</pre>";
+function rebuildAiTable(currentDateString) {
+    if(editor == null) {
+        return [];
     }
-
-    ai_table = new Map();
+    let d = html_to_text.convert(editor.getData(), {wordwrap:9999999});
+    let ai_table = [];
     let lines = d.split('\n');
     let date = timeFormat();
+    let lncnt = 0;
 
     function addAI(number, state, owner, comment) {
+        // yes, I am intending to do lexicographical compares
+        if(currentDateString == null || currentDateString >= date) {
+            ai_table.push({
+                ln : lncnt,
+                date : date,
+                number : number,
+                state : state,
+                owner : owner,
+                comment : comment
+            });
+        }
     }
 
     lines.forEach(ln => {
+        lncnt++;
         let m = ln.match(/!!date\s*(.*)$/);
         if(m) {
             console.log("date", m[1]);
             date = m[1];
         }
 
-        let m2 = ln.match(/!!ai\s*\(([^|]*)\|([^|]*)\|([^|]*)\)(.*)$/);
+        let m2 = ln.match(/!!ai\s*\(([0-9]*)\|([^|]*)\|([^|]*)\)(.*)$/);
         if(m2) {
             console.log("ai", m2);
             addAI(m2[1], m2[2], m2[3], m2[4]);
         }
 
-        m2 = ln.match(/!!ai\s*\(([^|]*)\|([^|]*)\)(.*)$/);
+        m2 = ln.match(/!!ai\s*\(([0-9]*)\|([^|]*)\)(.*)$/);
         if(m2) {
             console.log("ai", m2);
             addAI(m2[1], m2[2], "", m2[3]);
         }
-        m2 = ln.match(/!!ai\s*\(([^|]*)\)(.*)$/);
+        m2 = ln.match(/!!ai\s*\(([0-9]*)\)(.*)$/);
         if(m2) {
             console.log("ai", m2);
             addAI(m2[1], "", "", m2[3]);
         }
     });
+    ai_table.sort( (a, b) => {
+        if(a["number"] > b["number"]) return 1;
+        else if(a["number"] < b["number"]) return -1;
+        else if(a["date"] > b["date"]) return 1;
+        else if(a["date"] < b["date"]) return -1;
+        else if(a["ln"] > b["ln"]) return 1;
+        else if(a["ln"] < b["ln"]) return -1;
+        else return 0;
+    });
+    return ai_table;
+}
+
+function getNextAINum() {
+    let ai_table = rebuildAiTable(null);
+    if(ai_table.length == 0) return 1;
+    return 1 + Math.max(...(ai_table.map(x => parseInt(x["number"]))));
+}
+
+function updateChildWindow() {
+    let d = html_to_text.convert(editor.getData(), {wordwrap:9999999});
+    if(getWindow() != window) {
+        getWindow().msgpane.innerHTML = "<pre>" + d + "</pre>";
+    }
+}
+
+function buildAiTableHtml() {
+    // first thing to do is to find the date of the section to generate the AI html.
+    // This is done by scanning through the text to find !!aitable tag,
+    // and find the last '!!date' tag.
+    // Default is to keep the 'date' tag nullptr (can happen, which will be all AIs)
+
+    if(editor == null) {
+        return "";
+    }
+
+    let p1 = editor.model.createPositionAt(editor.model.document.getRoot(), 0);
+    let p2 = editor.model.document.selection.getFirstPosition();
+    let r = editor.model.createRange(p1, p2);
+
+    for(let x of r.getItems()) {
+        if(x["textNode"]) {
+            let ln = x["data"];
+            let m = ln.match(/!!date\s*(.*)$/);
+            if(m) {
+                date = m[1];
+            }
+        }
+    }
+    console.log(date);
+
+    let aitable = rebuildAiTable(date);
+    let ret = "<table>";
+    ret += "<tr><td>#</td><td>state</td><td>owner</td><td>description</td></tr>";
+    let prev_num = null;
+    for(let ln of aitable) {
+        let ln_date = ln["date"].split(" ")[0];
+        let ln_num = ln["number"];
+        if(ln_num == prev_num) {
+            ln_num = "";
+        } else {
+            prev_num = ln_num;
+        }
+        ret += "<tr><td>" + ln_num + "</td><td>" + ln["state"] + "</td><td>" + ln["owner"] + "</td><td>(" + ln_date + ") " + ln["comment"] +"</td></tr>";
+    }
+    ret += "</table>";
+
+    let pos = editor.model.document.selection.getFirstPosition();
+    let viewFragment = editor.data.processor.toView(ret);
+    let modelFragment = editor.data.toModel(viewFragment);
+    editor.model.insertContent(modelFragment, pos);
+
+    return "";
 }
 
 function createEditor() {
-
     ClassicEditor
         .create( document.querySelector('#editor'), {
             simpleUpload: {
@@ -153,11 +231,15 @@ function createEditor() {
                     extra: [
                         {
                             from: /^(!!date\s+)(##)$/,
-                            to: [null, timeFormat()],
+                            to: matches => [null, timeFormat()],
                         },
                         {
                             from: /(!!ai\s*\()(#)(\|?[^|]*\|?[^|]*\))$/,
-                            to: [null, new String(getNextAINum()), null],
+                            to: matches => [null, new String(getNextAINum()), null],
+                        },
+                        {
+                            from: /(!!aitable\s*)(##)$/,
+                            to: matches => [null, buildAiTableHtml()],
                         }
                     ],
                 }
